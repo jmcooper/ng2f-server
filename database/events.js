4a3699415ca65fceeed98f7fcdc7b298e6abc6df
Jim Cooper <jim@cooperplanet.com>
2017-10-16 17:21:57 -0600

    Update asset paths for event images

--- database/events.js
+++ database/events.js
@@ -6,7 +6,7 @@ var events = [
     date: '9/26/2036',
     time: '10:00 am',
     price: 599.99,
-    imageUrl: '/app/assets/images/angularconnect-shield.png',
+    imageUrl: '/assets/images/angularconnect-shield.png',
     location: {
       address: '1057 DT',
       city: 'London',
@@ -84,7 +84,7 @@ var events = [
     date: '4/15/2037',
     time: '9:00 am',
     price: 950.00,
-    imageUrl: '/app/assets/images/ng-nl.png',
+    imageUrl: '/assets/images/ng-nl.png',
     onlineUrl: "http://ng-nl.org/",
     sessions: [
       {
@@ -140,7 +140,7 @@ var events = [
     date: '5/4/2037',
     time: '9:00 am',
     price: 759.00,
-    imageUrl: '/app/assets/images/ng-conf.png',
+    imageUrl: '/assets/images/ng-conf.png',
     location: {
       address: 'The Palatial America Hotel',
       city: 'Salt Lake City',
@@ -222,7 +222,7 @@ var events = [
     date: '6/10/2037',
     time: '8:00 am',
     price: 800.00,
-    imageUrl: '/app/assets/images/basic-shield.png',
+    imageUrl: '/assets/images/basic-shield.png',
     location: {
       address: 'The UN Angular Center',
       city: 'New York',
@@ -271,7 +271,7 @@ var events = [
     date: '2/10/2037',
     time: '9:00 am',
     price: 400.00,
-    imageUrl: '/app/assets/images/ng-vegas.png',
+    imageUrl: '/assets/images/ng-vegas.png',
     location: {
       address: 'The Excalibur',
       city: 'Las Vegas',
